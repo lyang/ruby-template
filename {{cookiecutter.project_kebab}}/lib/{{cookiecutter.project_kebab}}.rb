e20f2eacf5013227360c07f947eff0c9394011ae
Lin Yang <github@linyang.me>
2021-05-15 21:26:26 -0700

    Re-enable Style/Documentation check

--- {{cookiecutter.project_kebab}}/lib/{{cookiecutter.project_kebab}}.rb
+++ {{cookiecutter.project_kebab}}/lib/{{cookiecutter.project_kebab}}.rb
@@ -2,5 +2,7 @@
 
 require_relative '{{cookiecutter.project_kebab}}/version'
 
+# {{cookiecutter.project_summary}}
+# @author {{cookiecutter.author_name}}
 module {{cookiecutter.project_pascal}}
 end
